c74a1d8847647dbd9a657c158606406042f35194
Jelle VdP <jellevandepoel@gmail.com>
2022-05-19 09:41:27 +0200

    reduced the switch statements, inserted the restart button and restart functionality, added some extra responsive design

--- script.js
+++ script.js
@@ -9,9 +9,10 @@ const options = [
 const yourChoiceEl = document.querySelector(".your-choice__output");
 const choiceButtons = document.querySelectorAll(".your-choice__button-wrapper button");
 const playGameButton = document.querySelector(".play-game");
+const resetGameButton = document.querySelector(".reset-game");
 const outputYourChoiceEl = document.querySelector(".game-result__you .game-result__choice");
 const outputOpponentChoiceEl = document.querySelector(".game-result__opponent .game-result__choice");
-const choicesEl = document.querySelector(".game-result")
+const choicesEl = document.querySelector(".game-result");
 const gameWinnerEl = document.querySelector(".game-winner");
 
 let yourChoice = false;
@@ -23,99 +24,79 @@ const generateResult = (you, opponent) => {
         case "Rock":
             switch (opponent) {
                 case "Rock":
-                    gameWinnerEl.innerText = "It's a draw!"
+                    gameWinnerEl.innerText = "It's a draw!";
                 break;
                 case "Paper":
-                    gameWinnerEl.innerText = "Your opponent wins!"
+                case "Spock":
+                    gameWinnerEl.innerText = "Your opponent wins!";
                 break;
                 case "Scissors":
-                    gameWinnerEl.innerText = "You win!"
-                break;
                 case "Lizard":
-                    gameWinnerEl.innerText = "You win!"
+                    gameWinnerEl.innerText = "You win!";
                 break;
-                case "Spock":
-                    gameWinnerEl.innerText = "Your opponent wins!"
-                break;
-            }
+            };
         break;
         case "Paper":
             switch (opponent) {
                 case "Rock":
-                    gameWinnerEl.innerText = "You win!"
+                case "Spock":
+                    gameWinnerEl.innerText = "You win!";
                 break;
                 case "Paper":
-                    gameWinnerEl.innerText = "It's a draw!"
+                    gameWinnerEl.innerText = "It's a draw!";
                 break;
                 case "Scissors":
-                    gameWinnerEl.innerText = "Your opponent wins!"
-                break;
                 case "Lizard":
-                    gameWinnerEl.innerText = "Your opponent wins!"
+                    gameWinnerEl.innerText = "Your opponent wins!";
                 break;
-                case "Spock":
-                    gameWinnerEl.innerText = "You win!"
-                break;
-            }
+            };
         break;
         case "Scissors":
             switch (opponent) {
                 case "Rock":
-                    gameWinnerEl.innerText = "Your opponent wins!"
+                case "Spock":
+                    gameWinnerEl.innerText = "Your opponent wins!";
                 break;
                 case "Paper":
-                    gameWinnerEl.innerText = "You win!"
+                case "Lizard":
+                    gameWinnerEl.innerText = "You win!";
                 break;
                 case "Scissors":
-                    gameWinnerEl.innerText = "It's a draw!"
-                break;
-                case "Lizard":
-                    gameWinnerEl.innerText = "You win!"
-                break;
-                case "Spock":
-                    gameWinnerEl.innerText = "Your opponent wins!"
+                    gameWinnerEl.innerText = "It's a draw!";
                 break;
             }
         break;
         case "Lizard":
             switch (opponent) {
                 case "Rock":
-                    gameWinnerEl.innerText = "Your opponent wins!"
+                case "Scissors":
+                    gameWinnerEl.innerText = "Your opponent wins!";
                 break;
                 case "Paper":
-                    gameWinnerEl.innerText = "You win!"
-                break;
-                case "Scissors":
-                    gameWinnerEl.innerText = "Your opponent wins!"
+                case "Spock":
+                    gameWinnerEl.innerText = "You win!";
                 break;
                 case "Lizard":
-                    gameWinnerEl.innerText = "It's a draw!"
+                    gameWinnerEl.innerText = "It's a draw!";
                 break;
-                case "Spock":
-                    gameWinnerEl.innerText = "You win!"
-                break;
-            }
+            };
         break;
         case "Spock":
             switch (opponent) {
                 case "Rock":
-                    gameWinnerEl.innerText = "You win!"
+                case "Scissors":
+                    gameWinnerEl.innerText = "You win!";
                 break;
                 case "Paper":
-                    gameWinnerEl.innerText = "Your opponent wins!"
-                break;
-                case "Scissors":
-                    gameWinnerEl.innerText = "You win!"
-                break;
                 case "Lizard":
-                    gameWinnerEl.innerText = "Your opponent wins!"
+                    gameWinnerEl.innerText = "Your opponent wins!";
                 break;
                 case "Spock":
-                    gameWinnerEl.innerText = "It's a draw!"
+                    gameWinnerEl.innerText = "It's a draw!";
                 break;
-            }
+            };
         break;
-    }
+    };
 };
 
 choiceButtons.forEach(button => {
@@ -129,10 +110,21 @@ playGameButton.addEventListener("click", () => {
     if (!yourChoice) {
         window.alert("Make a choice");
     } else {
+        playGameButton.style.display = "none";
+        resetGameButton.style.display = "inline-block";
         choicesEl.style.display = "flex";
         outputYourChoiceEl.innerText = yourChoice;
         let opponentsChoice = generateOpponentMove();
         outputOpponentChoiceEl.innerText = opponentsChoice;
         generateResult(yourChoice, opponentsChoice);
     }
+});
+
+resetGameButton.addEventListener("click", () => {
+    playGameButton.style.display = "inline-block";
+    resetGameButton.style.display = "none";
+    choicesEl.style.display = "none";
+    yourChoice = false;
+    yourChoiceEl.innerText = "";
+    gameWinnerEl.innerText = "";
 });
